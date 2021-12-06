fb99b10c1eba7ebae98f06e1ddf611e1949e9fa2
n-yako <n-yako@yandex-team.ru>
2021-12-07 00:56:03 +0300

    added BlocObserver

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'app/app.dart';
 
 void main() {
-  runApp(const NasaFeedApp());
+  BlocOverrides.runZoned(
+    () => runApp(const NasaFeedApp()),
+    blocObserver: _MyBlocObserver(),
+  );
+}
+
+class _MyBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    print(event);
+    super.onEvent(bloc, event);
+  }
 }
